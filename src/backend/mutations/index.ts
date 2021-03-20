14bda0951a87d91d342dc284a2d0c1236d6ac253
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-03-20 19:14:02 +0100

    customers mutations

--- src/backend/mutations/index.ts
+++ src/backend/mutations/index.ts
@@ -1,1 +1,23 @@
+import { mutationType } from "nexus";
+
 export * from "./auth/login";
+
+export const customers = mutationType({
+  definition(t) {
+    t.crud.createOneCustomer({
+      computedInputs: {
+        createdBy: async ({ ctx }) => {
+          const currentUser = await ctx.user.getCurrentUser();
+
+          return {
+            connect: {
+              id: currentUser.id,
+            },
+          };
+        },
+      },
+    });
+    t.crud.updateOneCustomer();
+    t.crud.deleteOneCustomer();
+  },
+});
