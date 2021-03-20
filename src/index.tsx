b2b394a6f3331f3ad5dcf98fdda71c5b84438be0
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-03-20 12:05:15 +0100

    added parcel bundler

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+
+const Application: React.FC<{}> = () => {
+    const [a, setA] = useState(0);
+    return (
+        <>
+        <h1>Application Whatever {a}</h1>
+        <button onClick={()=>setA(a+1)}>+</button>
+        <button onClick={()=>setA(a-1)}>-</button>
+        </>
+      );
+}
+
+render(<Application />, document.getElementById('root'));
