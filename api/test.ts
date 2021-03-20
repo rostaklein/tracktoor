e06088382f38b706af89f70114e34f2054c33d28
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-03-20 16:13:28 +0100

    added nexus

--- api/test.ts
+++ api/test.ts
@@ -1,5 +1,8 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { printSchema } from "graphql";
+
+import schema from "../src/backend/schema";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  return res.status(200).json({ hello: "Hello!", method: req.method });
+  return res.status(200).send(`<pre>${printSchema(schema)}</pre>`);
 };
