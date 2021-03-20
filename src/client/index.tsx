cb63f8432794910146dc7b9441c45b09143696b1
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-03-20 23:08:40 +0100

    added ant design

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,13 +1,18 @@
+import { Button } from "antd";
 import React, { useState } from "react";
 import { render } from "react-dom";
+
+import "./style/index.less";
 
 const Application: React.FC<{}> = () => {
   const [a, setA] = useState(0);
   return (
     <>
       <h1>Application Whatever {a}</h1>
-      <button onClick={() => setA(a + 1)}>+</button>
-      <button onClick={() => setA(a - 1)}>-</button>
+      <Button onClick={() => setA(a + 1)} type="primary">
+        +
+      </Button>
+      <Button onClick={() => setA(a - 1)}>-</Button>
     </>
   );
 };
